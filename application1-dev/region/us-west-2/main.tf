a6fa1e1fb11778afd3e83bb53f444593559df12b
Sureya Sathiamoorthi <sureya.sathiamoorthi@cloudreach.com>
2019-09-13 14:06:45 +0100

    working code updated

--- application1-dev/region/us-west-2/main.tf
+++ application1-dev/region/us-west-2/main.tf
@@ -16,5 +16,8 @@ provider "aws" {
 
 module "ec2-prod-instance" {
   source = "github.com/Sureya/tf-modules/ec2-module"
+//  Other accepted source values for local & gitlab sources
+//  source = "/can/be/a/relative/path/to/the/folder"
+//  source = "git::ssh://git@git.sami.int.thomsonreuters.com/gcp-terraform-modules/centralised-logging.git?ref=develop"
   instance_name = "prod-instance"
 }
